c973873ac16aa402b92af75915619dbc367c7340
halyna1995 <halyna.romanyshyn@gmail.com>
2017-07-19 20:59:26 +0300

    user input

--- Calculate.java
+++ Calculate.java
@@ -1,6 +1,18 @@
 public class Calculate
 {
   public static void main(String[] arg){
-System.out.println("calculate...");
+	System.out.println("calculate...");
+	int first=Integer.valueOf(arg[0]);
+	int second=Integer.valueOf(arg[1]);
+	int summ=first+second;
+	int rizn=first-second;
+	int mnozhen=first*second;
+	int dilennya=first/second;
+	double pidn_do_stepennya=Math.pow(first, second);
+	System.out.println("Suma=  "+summ);
+	System.out.println("Riznycya=  "+rizn);
+	System.out.println("Mnozhennya=  "+mnozhen);
+	System.out.println("Dilennya=  "+dilennya);
+	System.out.println("Pidnesennya do stepennya=  "+pidn_do_stepennya);
 }
 };
