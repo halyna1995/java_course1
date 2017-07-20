cef291804f04b0f34db09233f3126d72e95a578f
halyna1995 <halyna.romanyshyn@gmail.com>
2017-07-20 11:57:54 +0300

    user input

--- Calculator.java
+++ Calculator.java
@@ -6,6 +6,29 @@ public class Calculator
 	this.result+=param;
 }
 }
+public void mins(int... params) {
+        this.result = params[0];
+        for (int i = 1; i < params.length; i++) {
+            this.result -= params[i];
+        }
+    }
+
+public void mnoz(int... params) {
+        this.result = params[0];
+        for (int i = 1; i < params.length; i++) {
+            this.result *= params[i];
+        }
+    }
+
+public void dil(int... params)  {
+        this.result = params[0];
+        for (int i = 1; i < params.length; i++) {
+            if (params[i] == 0) {
+                System.out.println("Division by zero!!!");
+            }
+            this.result /= params[i];
+        }
+    }
   public int getResult(){
       return this.result;
 }
--- InteractRunner.java
+++ InteractRunner.java
@@ -11,8 +11,34 @@ public class  InteractRunner{
 		String first=reader.next();
 		System.out.println("Enter second arg: ");
 		String second=reader.next();
-		calc.add(Integer.valueOf(first),Integer.valueOf(second));
+		
+                System.out.println("chose operation: 1-> + 2-> - 3-> * 4-> /");
+		int a=reader.nextInt();
+		
+		System.out.println("a=   "+a);
+/**switch
+case 1:
+                //Добавление
+                add(Integer.valueOf(first), Integer.valueOf(second));
+                break;
+*/
+
+if(a == 1) {
+		calc.add(Integer.valueOf(first),Integer.valueOf(second));		
+}
+else if (a == 2){
+		calc.mins(Integer.valueOf(first),Integer.valueOf(second));
+}
+else if (a == 3){
+		calc.mnoz(Integer.valueOf(first),Integer.valueOf(second));
+}else if (a == 4){
+		calc.dil(Integer.valueOf(first),Integer.valueOf(second));
+}
+
 		System.out.println("Result: "+calc.getResult());
+		
+
+
 		calc.clearResult();
 		System.out.println("Exit: yes/no");
 		exit=reader.next();
